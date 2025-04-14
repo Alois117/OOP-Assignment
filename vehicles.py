6ac03117a322c62359c2b5ae215b573349ce94ef
Alois117 <aloismbithi01@gmail.com>
2025-04-14 12:09:03 +0300

    Initial Commit

---
+++ vehicles.py
@@ -0,0 +1,22 @@
+class Vehicle:
+    def move(self):
+        print("The vehicle is moving")
+
+class Car(Vehicle):
+    def move(self):
+        print("Driving 🚗")
+
+class Plane(Vehicle):
+    def move(self):
+        print("Flying ✈️")
+
+class Boat(Vehicle):
+    def move(self):
+        print("Sailing 🚤")
+
+
+# Example usage
+if __name__ == "__main__":
+    vehicles = [Car(), Plane(), Boat()]
+    for v in vehicles:
+        v.move()
